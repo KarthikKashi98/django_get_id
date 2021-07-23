fc33f3d60fb79ce9f97e234a84457c580f86b728
KarthikKashi98 <karthikkashi98@gmail.com>
2021-07-24 00:25:52 +0530

    Initial commit

---
+++ task_new/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from .import views
+urlpatterns = [
+   path('',views.apiOverview,name="api-overview"),
+   path('task-list/', views.taskList, name="task")
+
+]
