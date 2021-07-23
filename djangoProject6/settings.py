5e43489472c15903d9e09696a9eaeed6914ebaa3
KarthikKashi98 <karthikkashi98@gmail.com>
2021-07-24 01:04:29 +0530

    commit

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -126,3 +126,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DISABLE_COLLECTSTATIC=1
